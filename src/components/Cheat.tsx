dd5c968dcc831f1e1976b3eddbfb121a3a07eb10
DevonRobitaille <devon.robitaille+github@protonmail.com>
2022-07-07 16:13:45 -0400

    Completed Cheat.tsx

--- src/components/Cheat.tsx
+++ src/components/Cheat.tsx
@@ -5,8 +5,25 @@ interface IProps {
 }
 
 const Cheat: NextPage<IProps> = ({ eliminatedChar }) => {
+    const alpha: number[] = Array.from(Array(26)).map((e, i) => i + 65);
+    const alphabet: string[] = alpha.map((x) => String.fromCharCode(x));
+
     return (
-        <div>Cheat</div>
+        <div className='grid grid-cols-5 mx-5'>
+            {
+                alphabet.map((char, index: number) => {
+                    return (
+                        <>
+                            {(index === 25) && <div className='col-span-2' />}
+                            <div className={`
+                                card text-center
+                                ${eliminatedChar.has(char) ? "bg-[#CCC] text-[#BBB] " : "bg-white text-black "}
+                            `}>{char}</div>
+                        </>
+                    )
+                })
+            }
+        </div>
     )
 }
 
