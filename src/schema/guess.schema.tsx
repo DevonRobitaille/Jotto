7c9ed1f7ecb07586daff1e62354aa7f728c460c0
DevonRobitaille <devon.robitaille+github@protonmail.com>
2022-07-07 15:43:43 -0400

    Added regex to schema

--- src/schema/guess.schema.tsx
+++ src/schema/guess.schema.tsx
@@ -1,13 +1,15 @@
 import * as z from 'zod'
 
+const wordRegex = new RegExp('^[a-zA-Z]{5}$')
+
 export const guessSchema = z.object({
-    word: z.string().length(5)
+    word: z.string().regex(wordRegex)
 })
 
 export type Guess = z.TypeOf<typeof guessSchema>
 
 export const guessListSchema = z.object({
-    word: z.string().length(5),
+    word: z.string().regex(wordRegex),
     score: z.number().lte(5).gte(0),
     correct: z.boolean()
 }).array()
