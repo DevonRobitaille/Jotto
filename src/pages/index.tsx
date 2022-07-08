b1cca5eb209842cc2148c25196add1b32081693e
DevonRobitaille <46681410+DevonRobitaille@users.noreply.github.com>
2022-07-08 17:19:09 -0400

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,31 +14,52 @@ const Home: NextPage = () => {
   const [answer, setAnswer] = useState<string | undefined>(undefined)
   const [playerEliminatedList, setPlayerEliminatedList] = useState<Set<string>>(new Set())
 
+  let gameOver: string | null = null;
+  guesses.forEach((guess) => {
+    if (guess.correct) gameOver = guess.word;
+  })
+
   return (
-    <div className='w-screen max-w-sm h-screen mx-auto relative'>
-      {/* Header */}
-      <Header showCheatPage={showCheatPage} setShowCheatPage={setShowCheatPage} />
+    <>
+      {
+        (gameOver)
+          ? (
+            <>
+              < Header showCheatPage={showCheatPage} setShowCheatPage={setShowCheatPage} />
+              <div>
+                <h1 className='text-center text-3xl mt-40'>The word was <span className='font-bold text-score-0'>'{gameOver}'</span></h1>
+              </div>
+            </>
+          ) : (
+            < div className='w-screen max-w-sm h-screen mx-auto relative' >
+              {/* Header */}
+              < Header showCheatPage={showCheatPage} setShowCheatPage={setShowCheatPage} />
 
-      {showCheatPage
-        ? (
-          <Cheat eliminatedList={eliminatedList} correctList={correctList} playerEliminatedList={playerEliminatedList} setPlayerEliminatedList={setPlayerEliminatedList} />
-        ) : (
-          <>
-            {/* Score / Result header */}
-            < div className='space-y-5 mx-auto flex flex-col mb-[120px]'>
-              {guesses && guesses.map((guess, index) => (
-                <Row playerEliminatedList={playerEliminatedList} key={guess.word + index} guess={guess} guessList={guesses} setGuesses={setGuesses} eliminatedList={eliminatedList} correctList={correctList} />
-              ))}
-            </div>
+              {
+                showCheatPage
+                  ? (
+                    <Cheat eliminatedList={eliminatedList} correctList={correctList} playerEliminatedList={playerEliminatedList} setPlayerEliminatedList={setPlayerEliminatedList} />
+                  ) : (
+                    <>
+                      {/* Score / Result header */}
+                      < div className='space-y-5 mx-auto flex flex-col mb-[120px]' >
+                        {guesses && guesses.map((guess, index) => (
+                          <Row playerEliminatedList={playerEliminatedList} key={guess.word + index} guess={guess} guessList={guesses} setGuesses={setGuesses} eliminatedList={eliminatedList} correctList={correctList} />
+                        ))
+                        }
+                      </div >
 
 
-            {/* Input */}
-            <Input playerEliminatedList={playerEliminatedList} setPlayerEliminatedList={setPlayerEliminatedList} guessList={guesses} answer={answer} setAnswer={setAnswer} setGuessList={setGuesses} eliminatedList={eliminatedList} setEliminatedList={setEliminatedList} correctList={correctList} setCorrectList={setCorrectList} />
-          </>
-        )
+                      {/* Input */}
+                      < Input playerEliminatedList={playerEliminatedList} setPlayerEliminatedList={setPlayerEliminatedList} guessList={guesses} answer={answer} setAnswer={setAnswer} setGuessList={setGuesses} eliminatedList={eliminatedList} setEliminatedList={setEliminatedList} correctList={correctList} setCorrectList={setCorrectList} />
+                    </>
+                  )
+              }
+
+            </div >
+          )
       }
-
-    </div >
+    </>
   )
 }
 
