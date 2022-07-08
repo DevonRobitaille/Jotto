b1f46bc04c5db3cdefc52354472d19c63b8304d5
DevonRobitaille <46681410+DevonRobitaille@users.noreply.github.com>
2022-07-08 14:47:05 -0400

    Update Header.tsx
    Can now start a new game with the plus icon.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,8 @@
 import { NextPage } from 'next'
-import { ExclamationCircleIcon, QuestionMarkCircleIcon } from '@heroicons/react/outline'
+import { ExclamationCircleIcon, PlusIcon, QuestionMarkCircleIcon } from '@heroicons/react/outline'
 import { Dispatch, SetStateAction } from 'react';
 import { UncontrolledTooltip } from "reactstrap";
+import { useRouter } from 'next/router';
 
 interface IProps {
     showCheatPage: boolean;
@@ -9,16 +10,38 @@ interface IProps {
 }
 
 const Header: NextPage<IProps> = ({ showCheatPage, setShowCheatPage }) => {
+    const router = useRouter()
+    const newGame = () => {
+        router.reload()
+    }
+
     return (
         <header className='flex items-center mb-[30px]'>
             <div className='flex flex-1'>
                 <p className='bg-black text-white font-semibold ml-8 px-8 text-4xl'>JOTTO</p>
             </div>
-            <p id="TooltipExample">
+
+            {/* New Game */}
+            <p id="TooltipNewGame">
+                <PlusIcon onClick={() => newGame()} className='h-7 mr-3 text-black hover:cursor-pointer hover:text-[#CCC]' />
+            </p>
+            <UncontrolledTooltip
+                target="TooltipNewGame"
+                placement="bottom"
+                delay={500}
+            >
+                <p className='bg-black text-white border-2 border-white mr-2 mt-1 px-2 py-1 rounded-full text-sm items-center flex'>
+                    <ExclamationCircleIcon className='h-4 mr-1 text-score-1' />
+                    Start a new game.
+                </p>
+            </UncontrolledTooltip>
+
+            {/* Cheat Page */}
+            <p id="TooltipCheatPage">
                 <QuestionMarkCircleIcon onClick={() => setShowCheatPage(!showCheatPage)} className='h-7 mr-3 text-black hover:cursor-pointer hover:text-[#CCC]' />
             </p>
             <UncontrolledTooltip
-                target="TooltipExample"
+                target="TooltipCheatPage"
                 placement="bottom"
                 delay={500}
             >
