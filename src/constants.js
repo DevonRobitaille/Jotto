d7ac244526bdf785349874abe96be11b6de9f345
DevonRobitaille <46681410+DevonRobitaille@users.noreply.github.com>
2022-07-08 15:47:53 -0400

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
-export const baseUrl = process.env.VERCEL_URL
-    ? `https://${process.env.VERCEL_URL}`
+export const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL
+    ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
     : 'http://localhost:3000'
 
 export const url = `${baseUrl}/api/trpc`
