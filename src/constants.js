69cbe96091db481fd8141e1f15e2b8b9e378bd56
DevonRobitaille <46681410+DevonRobitaille@users.noreply.github.com>
2022-07-08 16:32:58 -0400

    Updated URL Constant

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
-export const baseUrl = process.env.URL
-    ? `https://${process.env.URL}`
+export const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL
+    ? `https://jotto.vercel.app`
     : 'http://localhost:3000'
 
 export const url = `${baseUrl}/api/trpc`
